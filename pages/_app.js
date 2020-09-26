17dc15373d394542df7f54c5764c7d19d0158d29
Marcelo Mena <marcelomena010@gmail.com>
2020-09-26 20:26:23 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
