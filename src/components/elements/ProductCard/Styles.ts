aa4fcc50c53ce8bbc8fdcdb3583361bb271af47b
Marcelo Mena <marcelomena010@gmail.com>
2020-11-15 19:13:48 -0300

    Adjust RroductCard body

--- src/components/elements/ProductCard/Styles.ts
+++ src/components/elements/ProductCard/Styles.ts
@@ -1,10 +1,29 @@
 import styled from 'styled-components';
 
 import { Card as CardComponent } from 'antd';
-import { CardProps } from 'antd/lib/card';
+import { CardProps, CardMetaProps } from 'antd/lib/card';
 
 export const Card: React.FC<CardProps> = styled(CardComponent)`
     width: 250px;
+
+    .ant-card-body {
+        padding: 1rem;
+        text-align: center;
+
+        h3 {
+            margin: 0;
+        }
+
+        span {
+            color: ${(props) => props.theme.colors.gray};
+        }
+
+        h4 {
+            color: ${(props) => props.theme.colors.primary_1};
+            font-weight: 500;
+            margin: 0;
+        }
+    }
 `;
 
-export const CardMeta: React.FC<CardProps> = styled(CardComponent.Meta)``;
+export const CardMeta: React.FC<CardMetaProps> = styled(CardComponent.Meta)``;
