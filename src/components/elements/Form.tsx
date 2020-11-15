2a18cc2975f0b52fa5fec7de2fbec3ce1216cdfb
Marcelo Mena <marcelomena010@gmail.com>
2020-11-15 01:37:50 -0300

    Add onFinish on form

--- src/components/elements/Form.tsx
+++ src/components/elements/Form.tsx
@@ -4,8 +4,12 @@ import { Form as Component } from 'antd';
 // PropsInterface
 import { FormProps } from '../../types/ElementProps';
 
-const Form: React.FC<FormProps> = ({ children, name }) => {
-    return <Component name={name}>{children}</Component>;
+const Form: React.FC<FormProps> = ({ children, name, onFinish }) => {
+    return (
+        <Component name={name} onFinish={onFinish}>
+            {children}
+        </Component>
+    );
 };
 
 export default Form;
